9688b4b84fbe8d5f62e2a6b89a0bf4be32ca8b5c
David Parlevliet <dave@leaguecraft.com>
2015-01-27 10:58:50 +1100

    fixed bug

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ wget --no-check-certificate https://raw.githubusercontent.com/dparlevliet/ssh-lo
 wget --no-check-certificate https://raw.githubusercontent.com/dparlevliet/ssh-login-notification/master/ssh_login.php -O /opt/ssh_login.php
 
 echo '** INSTALL COMPLETE ***********************************************************';
-if [ -f $FILE ];
+if [ -f $FILE ]; then
   echo '!IMPORTANT!';
   echo 'Please create the admin emails file necessary for sending out emails';
   echo '-'
